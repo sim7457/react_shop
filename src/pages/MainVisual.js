ec308ae55dd0a7a1c5c181650e8506890b5ff871
sim7457 <simwjdgns3@naver.com>
2023-03-03 14:36:34 +0900

    20230303 14:36

--- src/pages/MainVisual.js
+++ src/pages/MainVisual.js
@@ -1,28 +1,58 @@
 import "slick-carousel/slick/slick.css";
-import MainSlider from "react-slick";
+import MainSlide from "react-slick";
 import { BTN, Inner } from "./common";
-
-
+import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
 
 const MainVisual = ({ DEFAULT_CONTENTS }) => {
+    const option = {
+        arrows: false,
+        afterChange: idx => setSNum(idx)
+    }
+
+    const s = useRef(null);
+    const [sNum, setSNum] = useState(0)
+
     return (
         <>
-            <MainSlider className="MainSlider">
+            <MainSlide
+                {...option}
+                ref={s}
+                className={`MainSlide`}
+            >
                 {
                     DEFAULT_CONTENTS.map((slideItm, slideIndx) => {
                         return (
-                            <div className={`itm itm0${slideIndx + 1}`}>
+                            <div key={slideIndx} className={`itm itm0${slideIndx + 1} ${slideIndx === sNum ? 'on' : ''}`}>
                                 <Inner className="inner">
                                     <h2>{slideItm.title}</h2>
                                     <p>{slideItm.description}</p>
-                                    <BTN><Link to="/">more</Link></BTN>
+                                    <BTN color={slideItm.color}><Link to='/'>more</Link></BTN>
                                 </Inner>
-
                             </div>
                         )
                     })
                 }
-            </MainSlider>
+            </MainSlide>
+            <div className="arrows">
+                {console.log(s.current)}
+                <button onClick={() => s.current.slickPrev()}>뒤로가기</button>
+                <button onClick={() => s.current.slickNext()}>앞로가기</button>
+            </div>
+            <div className="num">
+                현재슬라이드의 번호 : {sNum + 1}
+                전체슬라이드갯수 : {DEFAULT_CONTENTS.length}
+            </div>
+
+            <ul>
+                {
+                    DEFAULT_CONTENTS.map((_, slideIndx) => {
+                        return (
+                            <li><button onClick={() => s.current.slickGoTo(slideIndx)}>{slideIndx + 1}</button></li>
+                        )
+                    })
+                }
+            </ul>
         </>
     )
 }
